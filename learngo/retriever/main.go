ae0481ce84605e379aa7a4b0534205ea2ee53f8b
wrq <pyq225@gmail.com>
2018-03-10 23:07:32 +0800

    add 5-3 interface value type

--- learngo/retriever/main.go
+++ learngo/retriever/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"learngo/retriever/mock"
 	"learngo/retriever/real"
+	"time"
 )
 
 type Retriever interface {
@@ -15,10 +16,43 @@ func download(r Retriever) string {
 }
 
 func main() {
-	//fmt.Println(download(mock.Retriever{"this is a fake imooc.com"}))
 	var r Retriever
+
 	r = mock.Retriever{"this is a fake imooc.com"} // 实现者只要实现Get方法
-	r = real.Retriever{}
-	fmt.Println(download(r))
+	inspect(r)
+	//mock.Retriever {this is a fake imooc.com}
+	//Contents: this is a fake imooc.com
+
+	r = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+	//*real.Retriever &{Mozilla/5.0 1m0s}
+	//UserAgent: Mozilla/5.0
+
+	// Type assertion 可以通过.(* 取得r里的类型
+	realRetriever := r.(*real.Retriever)
+	fmt.Println(realRetriever.TimeOut)
+	// 1m0s
+
+	if mockRetriever, ok := r.(mock.Retriever); ok {
+		fmt.Println(mockRetriever.Contents)
+	} else {
+		fmt.Println("not a mock retriever")
+	}
+	// not a mock retriever
+
+	// 接口变量里 可以有<实现者的类型> 或<实现者的指针>
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	switch v := r.(type) {
+	case mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent:", v.UserAgent)
+	}
 
 }
