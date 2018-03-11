f65ed00be4cd44598588b4f7f3c04ddbcb0e01c4
wrq <pyq225@gmail.com>
2018-03-11 13:13:09 +0800

    add 5-4 interface combination

--- learngo/retriever/main.go
+++ learngo/retriever/main.go
@@ -11,15 +11,41 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+const url = "http:// www.imooc.com"
+
 func download(r Retriever) string {
-	return r.Get("http:// www.imooc.com")
+	return r.Get(url)
+}
+
+func post(poster Poster) {
+	poster.Post(url,
+		map[string]string{
+			"name":   "wrq",
+			"course": "golang",
+		})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
 }
 
 func main() {
 	var r Retriever
 
-	r = mock.Retriever{"this is a fake imooc.com"} // 实现者只要实现Get方法
-	inspect(r)
+	retriever := mock.Retriever{"this is a fake imooc.com"} // 实现者只要实现Get方法
+	r = &retriever
 	//mock.Retriever {this is a fake imooc.com}
 	//Contents: this is a fake imooc.com
 
@@ -36,7 +62,7 @@ func main() {
 	fmt.Println(realRetriever.TimeOut)
 	// 1m0s
 
-	if mockRetriever, ok := r.(mock.Retriever); ok {
+	if mockRetriever, ok := r.(*mock.Retriever); ok {
 		fmt.Println(mockRetriever.Contents)
 	} else {
 		fmt.Println("not a mock retriever")
@@ -44,12 +70,15 @@ func main() {
 	// not a mock retriever
 
 	// 接口变量里 可以有<实现者的类型> 或<实现者的指针>
+
+	fmt.Println("Try a session")
+	fmt.Println(session(&retriever))
 }
 
 func inspect(r Retriever) {
 	fmt.Printf("%T %v\n", r, r)
 	switch v := r.(type) {
-	case mock.Retriever:
+	case *mock.Retriever:
 		fmt.Println("Contents:", v.Contents)
 	case *real.Retriever:
 		fmt.Println("UserAgent:", v.UserAgent)
