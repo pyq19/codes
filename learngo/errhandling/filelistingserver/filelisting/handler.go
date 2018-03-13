a745bcd1631c662704c2d3fb604465440b53b63b
wrq <pyq225@gmail.com>
2018-03-14 00:31:43 +0800

    add 7-5 web error handling 2

--- learngo/errhandling/filelistingserver/filelisting/handler.go
+++ learngo/errhandling/filelistingserver/filelisting/handler.go
@@ -4,11 +4,27 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+const prefix = "/list/"
 
-	path := request.URL.Path[len("/list/"):]
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
+func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("path must start with " + prefix)
+	}
+
+	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
 	if err != nil {
 		return err
--- learngo/errhandling/filelistingserver/web.go
+++ learngo/errhandling/filelistingserver/web.go
@@ -11,9 +11,22 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			log.Printf("Error handling request %s", err.Error())
+
+			if userError, ok := err.(userError); ok {
+				http.Error(writer, userError.Message(), http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -29,8 +42,13 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
