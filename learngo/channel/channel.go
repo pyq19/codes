d7c3c6965a5ed6e351a1b1f49985699709dcede5
wrq <pyq225@gmail.com>
2018-03-15 22:53:08 +0800

    add 10-1 channel

---
+++ learngo/channel/channel.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//func worker(id int, c chan int) {
+//	for {
+//		fmt.Printf("Worker %d received %c\n", id, <-c)
+//	}
+//}
+
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func chanDemo() {
+	var channels [10]chan<- int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+	time.Sleep(time.Millisecond)
+}
+
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
+}
+
+func channelClose() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	close(c) // 由发送方close
+	time.Sleep(time.Millisecond)
+}
+
+func main() {
+	fmt.Println("Channel as first-class citizen")
+	chanDemo()
+
+	fmt.Println("Buffered channel")
+	bufferedChannel()
+
+	fmt.Println("Channel close and range")
+	channelClose()
+}
