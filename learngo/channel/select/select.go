86f00854d28522a265840a54aec31f2508f1ff51
wrq <pyq225@gmail.com>
2018-03-17 14:30:44 +0800

    update 10-4 select

--- learngo/channel/select/select.go
+++ learngo/channel/select/select.go
@@ -22,6 +22,7 @@ func generator() chan int {
 
 func worker(id int, c <-chan int) {
 	for n := range c {
+		time.Sleep(time.Second)
 		fmt.Printf("Worker %d received %d\n", id, n)
 	}
 }
@@ -36,20 +37,30 @@ func main() {
 	var c1, c2 = generator(), generator()
 	var worker = createWorker(0)
 
-	n := 0
-	hasValue := false
+	var values []int
+	tm := time.After(9 * time.Second)
+	tick := time.Tick(time.Second)
 	for {
 		var activeWorker chan<- int
-		if hasValue {
+		var activeValue int
+		if len(values) > 0 {
 			activeWorker = worker
+			activeValue = values[0]
 		}
 		select {
-		case n = <-c1:
-			hasValue = true
-		case n = <-c2:
-			hasValue = true
-		case activeWorker <- n:
-			hasValue = false
+		case n := <-c1:
+			values = append(values, n)
+		case n := <-c2:
+			values = append(values, n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("timeout")
+		case <-tick:
+			fmt.Println("queue len =", len(values))
+		case <-tm:
+			fmt.Println("bye")
+			return
 		}
 	}
 }
