5984d24fa421fa522346ace946945b7a0805a423
wrq <pyq225@gmail.com>
2018-03-17 14:21:29 +0800

    add 10-4 select

---
+++ learngo/channel/select/select.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generator() chan int {
+	out := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+
+	return out
+}
+
+func worker(id int, c <-chan int) {
+	for n := range c {
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func main() {
+	var c1, c2 = generator(), generator()
+	var worker = createWorker(0)
+
+	n := 0
+	hasValue := false
+	for {
+		var activeWorker chan<- int
+		if hasValue {
+			activeWorker = worker
+		}
+		select {
+		case n = <-c1:
+			hasValue = true
+		case n = <-c2:
+			hasValue = true
+		case activeWorker <- n:
+			hasValue = false
+		}
+	}
+}
