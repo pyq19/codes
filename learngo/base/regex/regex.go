afbe79c5f189a498f7c03f9f7a5266b418f6afb0
wrq <pyq225@gmail.com>
2018-03-18 15:26:28 +0800

    update 14-2 regex findall 2

--- learngo/base/regex/regex.go
+++ learngo/base/regex/regex.go
@@ -9,12 +9,19 @@ const text = `
 my email is pyq225@gmail.com123
 email1 is abc@def.org
 email2 is kkk@aaa.com
+email3 is ddd@adb.com.cn
 `
 
 func main() {
-	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9\.]+\.[a-zA-Z0-9]+`)
-	match := re.FindAllString(text, -1)
+	re := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
+	match := re.FindAllStringSubmatch(text, -1)
 	fmt.Println(match)
-	// [pyq225@gmail.com123 abc@def.org kkk@aaa.com]
-
+	// [[pyq225@gmail.com123 pyq225 gmail .com123] [abc@def.org abc def .org] [kkk@aaa.com kkk aaa .com] [ddd@adb.com.cn ddd adb .com.cn]]
+	for _, m := range match {
+		fmt.Println(m)
+	}
+	// [pyq225@gmail.com123 pyq225 gmail .com123]
+	// [abc@def.org abc def .org]
+	// [kkk@aaa.com kkk aaa .com]
+	// [ddd@adb.com.cn ddd adb .com.cn]
 }
