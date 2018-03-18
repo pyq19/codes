fddf5298462e8f42c8bbe7eb2aae47c670303d13
wrq <pyq225@gmail.com>
2018-03-18 15:19:49 +0800

    update 14-1 regex findall

--- learngo/base/regex/regex.go
+++ learngo/base/regex/regex.go
@@ -5,11 +5,16 @@ import (
 	"regexp"
 )
 
-const text = "My email is pyq225@gmail.com123RR"
+const text = `
+my email is pyq225@gmail.com123
+email1 is abc@def.org
+email2 is kkk@aaa.com
+`
 
 func main() {
-	re := regexp.MustCompile(`[a-zA-Z0-9]+@.+\.[a-zA-Z0-9]+`)
-	match := re.FindString(text)
+	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9\.]+\.[a-zA-Z0-9]+`)
+	match := re.FindAllString(text, -1)
 	fmt.Println(match)
-	// pyq225@gmail.com123RR
+	// [pyq225@gmail.com123 abc@def.org kkk@aaa.com]
+
 }
