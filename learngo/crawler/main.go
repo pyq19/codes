0802e457eec7db34420ce3bad9320dd68d51abaf
wrq <pyq225@gmail.com>
2018-03-18 14:52:07 +0800

    update 14-1 determineEncoding

--- learngo/crawler/main.go
+++ learngo/crawler/main.go
@@ -5,7 +5,11 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"golang.org/x/text/encoding/simplifiedchinese"
+	"bufio"
+	"io"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
 	"golang.org/x/text/transform"
 )
 
@@ -20,8 +24,9 @@ func main() {
 		fmt.Println("Error: status code", resp.StatusCode)
 		return
 	}
+	e := determineEncoding(resp.Body)
 
-	utf8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
 
 	all, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
@@ -29,4 +34,13 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", all)
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
