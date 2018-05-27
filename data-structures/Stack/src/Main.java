345e14c01ac4e287071cb61e384e834babf4fa9e
wrq <pyq225@gmail.com>
2018-05-27 20:51:19 +0800

    add 3-2 Stack

--- data-structures/Stack/src/Main.java
+++ data-structures/Stack/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayStack<Integer> stack = new ArrayStack<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+        // Stack: [0] top
+        // Stack: [0, 1] top
+        // Stack: [0, 1, 2] top
+        // Stack: [0, 1, 2, 3] top
+        // Stack: [0, 1, 2, 3, 4] top
+        // Stack: [0, 1, 2, 3] top
     }
 }
