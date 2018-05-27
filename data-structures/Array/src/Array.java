4486be00b7f0f1d82d24b76d83e4d270d6191e74
wrq <pyq225@gmail.com>
2018-05-27 15:32:20 +0800

    add 2-3 add element to array

--- data-structures/Array/src/Array.java
+++ data-structures/Array/src/Array.java
@@ -23,9 +23,35 @@ public class Array {
         return data.length;
     }
 
+    // 返回数组是否为空
     public boolean isEmpty() {
         return size == 0;
     }
 
+    // 向所有元素后添加一个新元素
+    public void addList(int e) {
+        add(size, e);
+    }
 
+    // 在所有元素前添加一个新元素
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    // 在 index 个位置插入一个新元素 e
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("Add failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Required index >= 0 and index <= size.");
+        }
+
+        // 每个元素往后挪一个位置，直到 index
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
 }
