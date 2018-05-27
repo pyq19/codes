30ec78b3693dc30d1c5b9970aba906874b6d77db
wrq <pyq225@gmail.com>
2018-05-27 17:29:18 +0800

    2-9

--- data-structures/Array/src/Array.java
+++ data-structures/Array/src/Array.java
@@ -103,7 +103,7 @@ public class Array<E> {
         size--;
         data[size] = null; // loitering objects != memory leak
 
-        if (size == data.length / 2) {
+        if (size == data.length / 4 && data.length / 2 != 0) {
             resize(data.length / 2);
         }
 
