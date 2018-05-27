fa9c034d5456acefe6c61f3025299da92b13d36e
wrq <pyq225@gmail.com>
2018-05-27 15:10:47 +0800

    add imooc data-struct 2-1

--- data-structures/Array/src/Main.java
+++ data-structures/Array/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        int[] scores = new int[]{100, 99, 66};
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+        scores[0] = 98;
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
     }
 }
