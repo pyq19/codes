4c5e7bb5cfa17f4ce9d209ce98cf82847ac8c0cd
wrq <pyq225@gmail.com>
2018-05-27 17:02:59 +0800

    add 2-5 contain find remove

--- data-structures/Array/src/Array.java
+++ data-structures/Array/src/Array.java
@@ -68,6 +68,58 @@ public class Array {
         data[index] = e;
     }
 
+    // 查找数组中是否有元素 e
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素 e 所在的索引，如果不存在元素 e ，返回 -1
+    public int find(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 从数组中删除 index 位置的元素，返回删除的元素
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is illegal");
+        }
+        int ret = data[index];
+        // 0, 1, 2, 3, 4, 5, 6, 7
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        return ret;
+    }
+
+    // 从数组中删除第一个元素，返回删除的元素
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    // 从数组中删除最后一个元素，返回删除的元素
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从数组中删除元素 e
+    public void removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
--- data-structures/Array/src/Main.java
+++ data-structures/Array/src/Main.java
@@ -7,17 +7,29 @@ public class Main {
         }
 
         System.out.println(arr);
-        //Array: size = 10, capacity = 20
-        //[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        // Array: size = 10, capacity = 20
+        // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         arr.add(1, 100);
         System.out.println(arr);
-        //Array: size = 11, capacity = 20
-        //[0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        // Array: size = 11, capacity = 20
+        // [0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         arr.addFirst(-1);
         System.out.println(arr);
-        //Array: size = 12, capacity = 20
-        //[-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        // Array: size = 12, capacity = 20
+        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.remove(2);
+        System.out.println(arr);
+        // [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.removeElement(4);
+        System.out.println(arr);
+        // [-1, 0, 1, 2, 3, 5, 6, 7, 8, 9]
+
+        arr.removeFirst();
+        System.out.println(arr);
+        // [0, 1, 2, 3, 5, 6, 7, 8, 9]
     }
 }
