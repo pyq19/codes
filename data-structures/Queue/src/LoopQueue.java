5a75d88f823e5e7f42e1fa7ee87a52760b29ec58
wrq <pyq225@gmail.com>
2018-05-29 22:31:46 +0800

    add 3-6 LoopQueue

---
+++ data-structures/Queue/src/LoopQueue.java
@@ -0,0 +1,31 @@
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+}
