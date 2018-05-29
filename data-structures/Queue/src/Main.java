5f79b3ba1e7414f640c546e8da42710a65dcb28f
wrq <pyq225@gmail.com>
2018-05-30 00:38:06 +0800

    add 3-8 compare ArrayQueue and LoopQueue

--- data-structures/Queue/src/Main.java
+++ data-structures/Queue/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+
 public class Main {
+
+    // 测试使用 q 运行 opCount 个 enqueue 和 dequeue 操作所需要的时间，单位：秒
+    private static double testQueue(Queue<Integer> q, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+
+    }
+
     public static void main(String[] args) {
 
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<Integer>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+
+        /*
+        ArrayQueue, time: 4.30962955 s
+        LoopQueue, time: 0.017617984 s
+         */
     }
 }
