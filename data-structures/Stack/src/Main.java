dc532acc969bb66ee3460798e5e80a6411e2b447
wrq <pyq225@gmail.com>
2018-06-02 21:23:06 +0800

    add 4-6 LinkedListStack

--- data-structures/Stack/src/Main.java
+++ data-structures/Stack/src/Main.java
@@ -1,20 +1,35 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<Integer>();
 
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+        int opCount = 10000000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<Integer>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<Integer>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+
+//        ArrayStack, time: 2.894101289 s
+//        LinkedListStack, time: 4.877367154 s
+       
+//        linkedListStack 包含更多 new 操作
+    }
+
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
-
-        stack.pop();
-        System.out.println(stack);
-        // Stack: [0] top
-        // Stack: [0, 1] top
-        // Stack: [0, 1, 2] top
-        // Stack: [0, 1, 2, 3] top
-        // Stack: [0, 1, 2, 3, 4] top
-        // Stack: [0, 1, 2, 3] top
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
     }
 }
