c67a5c5d955305eeea6594575f1741c07857425e
wrq <pyq225@gmail.com>
2018-06-02 17:24:33 +0800

    add dummyHead to LinkedList

--- data-structures/LinkedList/src/LinkedList.java
+++ data-structures/LinkedList/src/LinkedList.java
@@ -23,11 +23,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead; // 虚拟头节点
     private int size;
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -41,17 +41,6 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    // 在链表头添加新的元素 e
-    public void addFirst(E e) {
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        // 等同于上三行
-        head = new Node(e, head);
-
-        size++;
-    }
 
     // 在链表的 index (0-based) 位置添加新的元素 e
     // 在链表中不是一个常用的操作，练习用
@@ -60,19 +49,15 @@ public class LinkedList<E> {
             throw new IllegalArgumentException("Add failed. Illegal index.");
         }
 
-        if (index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
-//            Node node = new Node(e);
-//            node.next = prev.next;
-//            prev.next = node;
-            prev.next = new Node(e, prev.next);
-            size++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+//      Node node = new Node(e);
+//      node.next = prev.next;
+//      prev.next = node;
+        prev.next = new Node(e, prev.next);
+        size++;
     }
 
     // 在链表末尾添加新的元素 e
@@ -80,4 +65,8 @@ public class LinkedList<E> {
         add(size, e);
     }
 
+    // 在链表头添加新的元素 e
+    public void addFirst(E e) {
+        add(0, e);
+    }
 }
