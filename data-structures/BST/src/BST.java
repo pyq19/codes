4c4700e2dff1921ab7b3266da6bcb0ec802a7128
wrq <pyq225@gmail.com>
2018-06-09 22:42:44 +0800

    add 6-3 add element to BST

--- data-structures/BST/src/BST.java
+++ data-structures/BST/src/BST.java
@@ -26,4 +26,35 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    // 向二分搜索树中添加新的元素 e
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    // 向以 node 为根的二分搜索树中插入元素 e，递归算法
+    public void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+
 }
