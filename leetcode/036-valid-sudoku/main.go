a22bfc784ab13adc1fdd9aef74e7095a880ec2ac
wrq <pyq225@gmail.com>
2018-07-24 18:20:36 +0800

    update

---
+++ leetcode/036-valid-sudoku/main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+TODO 有效的数独
+https://leetcode-cn.com/problems/valid-sudoku/description/
+*/
+
+func main() {}
+
+func isValidSudoku(board [][]byte) bool {
+
+	return true
+}
---
+++ leetcode/037-sudoku-solver/main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+TODO 解数独
+https://leetcode-cn.com/problems/sudoku-solver/description/
+*/
+
+func main() {}
+
+func solveSudoku(board [][]byte) {
+
+}
