e0c52eee8aaab6706d9f7d7b76e1e9e0bf1ce0fa
wrq <pyq225@gmail.com>
2018-07-30 19:11:12 +0800

    add quna 7-6 热销推荐组件

---
+++ quna/travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id">
+        <img class="item-img"
+             :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data() {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1506/30/7a528fae12c17e6e29d5d13a9d9813ee.water.jpg_200x200_e7f0d0ad.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1506/30/7a528fae12c17e6e29d5d13a9d9813ee.water.jpg_200x200_e7f0d0ad.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1506/30/7a528fae12c17e6e29d5d13a9d9813ee.water.jpg_200x200_e7f0d0ad.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+.title
+  margin-top: .2rem
+  line-height: .8rem
+  background: #eeeeee
+  text-indent: .2rem
+.item
+  display: flex
+  overflow: hidden
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height: .44rem
+      margin-top: .16rem
+      color: #fff
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius: .06rem
+</style>
