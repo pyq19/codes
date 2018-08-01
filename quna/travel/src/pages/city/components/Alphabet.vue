916e3ec92af989f244a53b6f4ae2bf58009df7a4
wrq <pyq225@gmail.com>
2018-08-02 00:40:29 +0800

    add quna 8-7 城市选择页 列表性能优化

--- quna/travel/src/pages/city/components/Alphabet.vue
+++ quna/travel/src/pages/city/components/Alphabet.vue
@@ -31,8 +31,13 @@ export default {
   },
   data() {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated() {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick(e) {
@@ -44,11 +49,15 @@ export default {
     },
     handleTouchMove(e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79 // 79 绿色区域的高度
-        const index = Math.floor((touchY - startY) / 20) // 20 每个字符高度  再向下取整
-        if ((index >= 0) & (index < this.letters.length))
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79 // 79 绿色区域的高度
+          const index = Math.floor((touchY - this.startY) / 20) // 20 每个字符高度  再向下取整
+          if ((index >= 0) & (index < this.letters.length))
+            this.$emit('change', this.letters[index])
+        }, 16)
       }
     },
     handleTouchEnd() {
