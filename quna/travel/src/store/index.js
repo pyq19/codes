da53d3c2412b4cb1b5fa018f9249542c74c15cc4
wrq <pyq225@gmail.com>
2018-08-07 14:02:23 +0800

    add quna 8-10 localStorage

--- quna/travel/src/store/index.js
+++ quna/travel/src/store/index.js
@@ -3,9 +3,16 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '上海'
+try {
+    if (localStorage.city) {
+        defaultCity = localStorage.city
+    }
+} catch (e) { }
+
 export default new Vuex.Store({
     state: {
-        city: '北京'
+        city: defaultCity
     },
     // actions: {
     //     changeCity (ctx, city) {
@@ -15,6 +22,9 @@ export default new Vuex.Store({
     mutations: {
         changeCity (state, city) {
             state.city = city
+            try {
+                localStorage.city = city
+            } catch (error) { }
         }
     }
 })
