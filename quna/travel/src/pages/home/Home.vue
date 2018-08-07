3662f1ca4a5751ce0b675dafe76f11a4c68fd5f0
wrq <pyq225@gmail.com>
2018-08-07 17:43:40 +0800

    add quna 8-11 使用keep-alive优化网页性能

--- quna/travel/src/pages/home/Home.vue
+++ quna/travel/src/pages/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <home-header :city="city"></home-header>
+    <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
     <home-recommend :list="recommendList"></home-recommend>
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,16 +27,19 @@ export default {
   },
   data() {
     return {
-      city: '',
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo() {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res) {
       res = res.data
@@ -51,7 +55,17 @@ export default {
     }
   },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated() {
+    console.log('--->', this.city)
+    console.log('===>', this.lastCity)
+    if (this.lastCity !== this.city) {
+      console.log('111')
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
