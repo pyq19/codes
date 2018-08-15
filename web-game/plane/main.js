e8d0641fa4180898c3a3e0ffa9f76b85f96db6f6
wrq <pyq225@gmail.com>
2018-08-16 00:18:07 +0800

    add plane load assets

--- web-game/plane/main.js
+++ web-game/plane/main.js
@@ -1,13 +1,61 @@
-// import Phaser from '../phaser.min.js'
+var game = new Phaser.Game(240, 400, Phaser.CANVAS, 'game')
 
-var game = new Phaser.Game(240, 400, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update })
+game.MyStates = {};
 
-function preload () {
-    console.log('preload');
+game.MyStates.boot = {
+    preload: function () {
+        game.load.image('preload', 'assets/preloader.gif');
+    },
+    create: function () {
+        game.state.start('load');
+    }
 }
-function create () {
-    console.log('create');
+
+game.MyStates.load = {
+    preload: function () {
+        var preloadSprite = game.add.sprite(game.width / 2 - 220 / 2, game.height / 2 - 19 / 2, 'preload');
+        game.load.setPreloadSprite(preloadSprite);
+        game.load.image('background', 'assets/bg.jpg');
+        game.load.image('copyright', 'assets/copyright.png');
+        game.load.spritesheet('myplane', 'assets/myplane.png', 40, 40, 4);
+        game.load.spritesheet('startbutton', 'assets/startbutton.png', 100, 40, 2);
+        game.load.spritesheet('replaybutton', 'assets/replaybutton.png', 80, 30, 2);
+        game.load.spritesheet('sharebutton', 'assets/sharebutton.png', 80, 30, 2);
+        game.load.image('mybullet', 'assets/mybullet.png');
+        game.load.image('bullet', 'assets/bullet.png');
+        game.load.image('enemy1', 'assets/enemy1.png');
+        game.load.image('enemy2', 'assets/enemy2.png');
+        game.load.image('enemy3', 'assets/enemy3.png');
+        game.load.spritesheet('explode1', 'assets/explode1.png', 20, 20, 3);
+        game.load.spritesheet('explode2', 'assets/explode2.png', 30, 30, 3);
+        game.load.spritesheet('explode3', 'assets/explode3.png', 50, 50, 3);
+        game.load.spritesheet('myexplode', 'assets/myexplode.png', 40, 40, 3);
+        game.load.image('award', 'assets/award.png');
+        game.load.audio('normalback', 'assets/normalback.mp3');
+        game.load.audio('playback', 'assets/playback.mp3');
+        game.load.audio('fashe', 'assets/fashe.mp3');
+        game.load.audio('crash1', 'assets/crash1.mp3');
+        game.load.audio('crash2', 'assets/crash2.mp3');
+        game.load.audio('crash3', 'assets/crash3.mp3');
+        game.load.audio('ao', 'assets/ao.mp3');
+        game.load.audio('pi', 'assets/pi.mp3');
+        game.load.audio('deng', 'assets/deng.mp3');
+        game.load.onFileComplete.add(function (process) {
+            console.log(process);
+        });
+    },
+    create: function () {
+        game.state.start('start')
+    },
 }
-function update () {
-    console.log('update');
+
+game.MyStates.start = {
+    create: function () {
+        game.add.sprite(0, 0, 'background');
+    },
 }
+
+game.state.add('boot', game.MyStates.boot);
+game.state.add('load', game.MyStates.load);
+game.state.add('start', game.MyStates.start);
+game.state.start('boot');
