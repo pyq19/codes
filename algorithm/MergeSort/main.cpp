22559d0dff35daf07a9d0265ab2d392180db1edd
wrq <pyq225@gmail.com>
2018-08-31 16:34:36 +0800

    add algo 3-3 归并排序法的优化

--- algorithm/MergeSort/main.cpp
+++ algorithm/MergeSort/main.cpp
@@ -48,13 +48,21 @@ void __merge(T arr[], int l, int mid, int r)
 template <typename T>
 void __mergeSort(T arr[], int l, int r)
 {
-    if (l >= r)
+    // if (l >= r)
+    //     return;
+
+    // 优化2
+    if (r - l <= 15)
+    {
+        insertionSort(arr, l, r);
         return;
+    }
 
     int mid = (l + r) / 2;
     __mergeSort(arr, l, mid);
     __mergeSort(arr, mid + 1, r);
-    __merge(arr, l, mid, r);
+    if (arr[mid] > arr[mid + 1]) // 优化1
+        __merge(arr, l, mid, r);
 }
 
 template <typename T>
@@ -78,6 +86,17 @@ int main()
     delete[] arr2;
 
     cout << endl;
+
+    int swapTimes = 10;
+    cout << "Test for Random Nearly Ordered Array, size = " << n << ", swap time = " << swapTimes << endl;
+    arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+    arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
+
+    delete[] arr1;
+    delete[] arr2;
 
     return 0;
 }
