b755872b3e7886942aa74549ecc3d7d4e43d91ee
wrq <pyq225@gmail.com>
2018-12-01 00:14:27 +0800

    add 5-6 商户投放子系统 - 服务接口值对象定义

---
+++ merchants/src/main/java/com/impyq/passbook/vo/Response.java
@@ -0,0 +1,24 @@
+package com.impyq.passbook.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    // 错误码, 正确返回 0
+    private Integer errorCode = 0;
+
+    // 错误信息, 正确返回空字符串
+    private String errorMsg = "";
+
+    // 返回值对象
+    private Object data;
+
+    // 正确的响应构造函数
+    public Response(Object data) {
+        this.data = data;
+    }
+}
