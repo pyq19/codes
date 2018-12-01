9d8102e94c6e784d0de7c16af336091b6d0ad0e0
wrq <pyq225@gmail.com>
2018-12-01 10:11:11 +0800

    add 5-12 商户投放子系统 - 注册拦截器

--- merchants/src/main/java/com/impyq/passbook/MerchantsApplication.java
+++ merchants/src/main/java/com/impyq/passbook/MerchantsApplication.java
@@ -1,12 +1,26 @@
 package com.impyq.passbook;
 
+import com.impyq.passbook.security.AuthCheckInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MerchantsApplication.class, args);
-	}
+    @Resource
+    private AuthCheckInterceptor authCheckInterceptor;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantsApplication.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor).addPathPatterns("/merchants/**"); // 配置拦截器拦截哪些请求
+        super.addInterceptors(registry); // 注册拦截器
+    }
 }
