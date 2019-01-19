a8fa57de21cc56d1fdb2e6d0eee3ae3d0a9cb7cb
pyq <pyq225@gmail.com>
2019-01-19 16:45:55 +0800

    update React 实战：设计模式和最佳实践ch02

--- basic_stop_watch/src/StopWatch.js
+++ basic_stop_watch/src/StopWatch.js
@@ -1,43 +1,76 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import MajorClock from './components/MajorClock';
+import ControlButtons from './components/ControlButtons';
+import SplitTimes from './components/SplitTimes';
+
 
 class StopWatch extends Component {
+  // constructor() {
+  //   super(...arguments);
+  //   this.onSplit = this.onSplit.bind(this);
+  //   this.onStart = this.onStart.bind(this);
+  //   this.onPause = this.onPause.bind(this);
+  //   this.onReset = this.onReset.bind(this);
+  // }
+
+  state = {
+    isStarted: false,
+    startTime: null,
+    currentTime: null,
+    splits: [],
+  }
+
+  onSplit = () => {
+    this.setState({
+      splits: [...this.state.splits, this.state.currentTime - this.state.startTime]
+    });
+  }
+
+
+  onStart = () => {
+    this.setState({
+      isStarted: true,
+      startTime: new Date(),
+      currentTime: new Date(),
+    });
+
+    this.intervalHandle = setInterval(() => {
+      this.setState({ currentTime: new Date() });
+    }, 1000 / 60);
+  }
+
+  onPause = () => {
+    clearInterval(this.intervalHandle);
+    this.setState({
+      isStarted: false,
+    });
+  }
+
+  onReset = () => {
+    this.setState({
+      startTime: null,
+      currentTime: null,
+      splits: [],
+    });
+  }
+
   render() {
     return (
-      <div>
-        <MajorClock></MajorClock>
-        <ControlButton></ControlButton>
-        <SplitTimes></SplitTimes>
-      </div>
+      <Fragment>
+        <MajorClock milliseconds={this.state.currentTime - this.state.startTime} />
+        <ControlButtons
+          activated={this.state.isStarted}
+          onStart={this.onStart}
+          onPause={this.onPause}
+          onSplit={this.onSplit}
+          onReset={this.onReset}
+        />
+        <SplitTimes value={this.state.splits} />
+      </Fragment>
     );
   }
 }
 
-const MajorClock = ({milliseconds}) => {
-  // TODO: 返回数字时钟JSX
-};
-
-const ControlButton = (props) => {
-  // TODO: 返回两个按钮JSX
-};
-
-const SplitTimes = (props) => {
-  // TODO: 返回所有计次时间JSX
-};
-
-MajorClock.propTypes = {
-  milliseconds: PropTypes.number.isRequired
-};
-
-ControlButton.propTypes = {
-  activated: PropTypes.bool,
-  onStart: PropTypes.func.isRequired,
-  onPause: PropTypes.func.isRequired,
-  onSplit: PropTypes.func.isRequired,
-  onReset: PropTypes.func.isRequired,
-};
-
-SplitTimes.propTypes = {
-  splits: PropTypes.arrayOf(PropTypes.number)
-};
 
 export default StopWatch;
---
+++ basic_stop_watch/src/components/SplitTimes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MajorClock from './MajorClock';
+
+const SplitTimes = ({value=[]}) => {
+    return value.map((v, k) => (
+        <MajorClock key={k} milliseconds={v} />
+    ));
+}
+
+export default SplitTimes;
