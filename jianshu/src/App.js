477b9a8b18e4223ff2a357523bd05cab008e6ecc
wrq <pyq225@gmail.com>
2019-02-15 22:27:43 +0800

    add react简书 7-2 使用 styled-components 完成 Header 组件布局 1

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Header from './common/header'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        hello world
-      </div>
+      <Header />
     );
   }
 }
---
+++ jianshu/src/common/header/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  HeaderWrapper,
+  Logo,
+  Nav
+} from './style';
+
+class Header extends Component {
+  render () {
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+
+        </Nav>
+      </HeaderWrapper>
+    )
+  }
+}
+
+export default Header;
