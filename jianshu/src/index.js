3c693bf4a05fa9faf0054dd748607e2f249a6809
wrq <pyq225@gmail.com>
2019-02-17 21:35:12 +0800

    add react简书 7-4 使用 iconfont 嵌入头部图标

--- jianshu/src/index.js
+++ jianshu/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style.js';
+import './style';
+import './statics/iconfont/iconfont';
 
 ReactDOM.render(<App />, document.getElementById('root'));
