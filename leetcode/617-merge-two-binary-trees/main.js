14c0e5a620760136db6f555f3066456b49db2249
wrq <pyq225@gmail.com>
2019-04-27 20:09:00 +0800

    add leetcode 617 TODO

---
+++ leetcode/617-merge-two-binary-trees/main.js
@@ -0,0 +1,16 @@
+// TODO https://leetcode.com/problems/merge-two-binary-trees/
+// Definition for a binary tree node.
+function TreeNode (val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (t1, t2) {
+
+};
