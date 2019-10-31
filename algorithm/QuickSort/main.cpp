94a915285c8e92905ac41e53e6f84f8191784f3b
pyq <pyq225@gmail.com>
2019-10-31 23:33:52 +0800

    add algo 3-8 三路快速排序法

--- algorithm/QuickSort/main.cpp
+++ algorithm/QuickSort/main.cpp
@@ -77,9 +77,59 @@ template <typename T> void quickSort2(T arr[], int n) {
   __quickSort2(arr, 0, n - 1);
 }
 
+/******** 三路快排 **************/
+// 将 arr[l...r] 分为 < v; == v; > v 三部分
+// 之后递归对 < v; > v 两部分继续进行三路快速排序
+template <typename T> void __quickSort3Ways(T arr[], int l, int r) {
+  if (r - l <= 15) {
+    insertionSort(arr, l, r);
+    return;
+  }
+
+  // partition
+  swap(
+      arr[l],
+      arr[rand() % (r - l + 1) + l]); // l 和 r 之间随机选择一个元素， 和 l 交换
+  T v = arr[l];
+  int lt = l;     // arr[l + 1...lt] < v
+  int gt = r + 1; // arr[gt...r] > v
+  int i = l + 1;  // arr[lt + 1 ... i) == v
+  while (i < gt) {
+    // TODO 这部分代码思路不清楚. 关键是索引没弄清楚.
+    // https://coding.imooc.com/lesson/71.html#mid=1461
+    // 三路快速排序 8:38
+    // arr[l+1...lt] < v; arr[lt+1...i-1] == v;   arr[gt...r] > v;
+    // arr = [.|.......|......|.......|.......]
+    //        v      lt        i       gt    r
+    // | 分割的 5 部分 分别是
+    // 1. v; 2. <v; 3. ==v; 4. 未处理; 5. >v
+    if (arr[i] < v) {
+      swap(arr[i], arr[lt + 1]);
+      lt++;
+      i++;
+    } else if (arr[i] > v) {
+      swap(arr[i], arr[gt - 1]);
+      gt--;
+    } else { // arr[i] == v
+      i++;
+    }
+  }
+  // 将 l 这个元素与 lt（等于 v）的这个元素交换
+  swap(arr[l], arr[lt]);
+  __quickSort3Ways(arr, l, lt - 1);
+  __quickSort3Ways(arr, gt, r);
+}
+
+template <typename T> void quickSort3Ways(T arr[], int n) {
+  srand(time(NULL)); // 使用随机的方式确定快速排序的标定点, 设立随机种子
+  __quickSort3Ways(arr, 0,
+                   n - 1); // arr: 要处理的数字, 0: 头位置, n - 1: 尾位置
+}
+
 int main() {
   int n = 1000000;
 
+  // 测试 1: 一般性测试
   cout << "Test for Random Array, size = " << n << ", random range [0, " << n
        << "]" << endl;
   int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
@@ -87,8 +137,8 @@ int main() {
   int *arr3 = SortTestHelper::copyIntArray(arr1, n);
 
   SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-  SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr3, n);
+  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr2, n);
+  SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
 
   delete[] arr1;
   delete[] arr2;
@@ -96,6 +146,7 @@ int main() {
 
   cout << endl;
 
+  // 测试 2: 测试近乎有序的数组
   int swapTimes = 100;
   cout << "Test for Random Nearly Ordered Array, size = " << n
        << ", swap time = " << swapTimes << endl;
@@ -104,8 +155,8 @@ int main() {
   arr3 = SortTestHelper::copyIntArray(arr1, n);
 
   SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-  SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr3, n);
+  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr2, n);
+  SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
 
   delete[] arr1;
   delete[] arr2;
@@ -113,6 +164,7 @@ int main() {
 
   cout << endl;
 
+  // 测试 3: 测试存在包含大量相同元素的数组
   cout << "Test for Random Array, size = " << n << ", random range [0, 10]"
        << endl;
   arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
@@ -120,8 +172,8 @@ int main() {
   arr3 = SortTestHelper::copyIntArray(arr1, n);
 
   SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-  // SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr3, n);
+  SortTestHelper::testSort("Quick Sort 2", quickSort2, arr2, n);
+  SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
 
   delete[] arr1;
   delete[] arr2;
