0c6e26e87a83864023d9c500fedc3ff30027d8ec
pyq <pyq225@gmail.com>
2019-11-05 18:10:02 +0800

    add leetcode 121 TODO

---
+++ leetcode/121-best-time-to-buy-and-sell-stock/main.py
@@ -0,0 +1,14 @@
+# coding:utf8
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+# TODO
+
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        return
+
+
+prices = [7, 6, 4, 3, 1]  # return 0
+prices = [7, 1, 5, 3, 6, 4]  # return 5
+print(Solution().maxProfit(prices))
