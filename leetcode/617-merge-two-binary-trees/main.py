f14484a1167ae96943032f71136ef18e92bdb0ff
pyq <pyq225@gmail.com>
2019-11-06 18:32:10 +0800

    add leetcode 617 TODO

---
+++ leetcode/617-merge-two-binary-trees/main.py
@@ -0,0 +1,18 @@
+# coding:utf8
+# https://leetcode.com/problems/merge-two-binary-trees/
+
+# TODO
+
+
+class TreeNode:
+    """Definition for a binary tree node."""
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        pass
