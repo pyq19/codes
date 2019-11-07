ba151cc4a76a7d7b7294cc4e83f8470431bf010e
pyq <pyq225@gmail.com>
2019-11-07 18:09:11 +0800

    update 104

---
+++ leetcode/104-maximum-depth-of-binary-tree/main.py
@@ -0,0 +1,16 @@
+# coding:utf8
+
+
+class TreeNode:
+    """Definition for a binary tree node."""
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        # TODO
+        pass
