10831f3b52c656878a48fbd36bbc225b3ab3a553
pyq <pyq225@gmail.com>
2019-11-08 17:56:18 +0800

    add leetcode 136 TODO

---
+++ leetcode/136-single-number/main.py
@@ -0,0 +1,11 @@
+# coding:utf8
+# https://leetcode.com/problems/single-number/
+
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # TODO
+        pass
