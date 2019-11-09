6f6644b248d0b07b9d688dfc97ed325b36274eaf
pyq <pyq225@gmail.com>
2019-11-09 17:02:34 +0800

    add leetcode 46 TODO

---
+++ leetcode/046-permutations/main.py
@@ -0,0 +1,26 @@
+# coding:utf8
+# https://leetcode.com/problems/permutations/
+
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # TODO
+        pass
+
+
+nums = [1, 2, 3]
+
+print(Solution().permute(nums))
+
+# Input: [1,2,3]
+# Output:
+# [
+#   [1,2,3],
+#   [1,3,2],
+#   [2,1,3],
+#   [2,3,1],
+#   [3,1,2],
+#   [3,2,1]
+# ]
