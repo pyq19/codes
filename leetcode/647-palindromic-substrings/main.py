32ea0ce3be93684c89a82aaaa2046d4216c5fc59
pyq <pyq225@gmail.com>
2019-11-09 18:06:44 +0800

    add leetcode 647 TODO

---
+++ leetcode/647-palindromic-substrings/main.py
@@ -0,0 +1,14 @@
+# coding:utf8
+# https://leetcode.com/problems/palindromic-substrings/
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        # TODO
+        pass
+
+
+s = "abc"  # 3
+print(Solution().countSubstrings(s))
+s = "aaa"  # 6
+print(Solution().countSubstrings(s))
