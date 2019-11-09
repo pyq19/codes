b0989f26e2008803ccbf055bd14697a1cb3c8b38
pyq <pyq225@gmail.com>
2019-11-09 15:52:41 +0800

    add leetcode 739 TODO

---
+++ leetcode/739-daily-temperatures/main.py
@@ -0,0 +1,15 @@
+# coding:utf8
+# https://leetcode.com/problems/daily-temperatures/
+
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        # TODO
+        pass
+
+
+T = [73, 74, 75, 71, 69, 72, 76, 73]  # return [1, 1, 4, 2, 1, 1, 0, 0]
+
+print(Solution().dailyTemperatures(T))
