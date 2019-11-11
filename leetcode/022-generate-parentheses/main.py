17db8093b6cc3bac87d2dd852190894f58717640
pyq <pyq225@gmail.com>
2019-11-11 18:17:32 +0800

    add leetcode 22 TODO

---
+++ leetcode/022-generate-parentheses/main.py
@@ -0,0 +1,14 @@
+# coding:utf8
+# https://leetcode.com/problems/generate-parentheses/
+
+
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # TODO
+        pass
+
+
+print(Solution().generateParenthesis(3))
