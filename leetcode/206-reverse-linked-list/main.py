ed6a183775bad4413dc55ce625c2229348912522
pyq <pyq225@gmail.com>
2019-11-12 12:25:40 +0800

    add leetcode 206 TODO

---
+++ leetcode/206-reverse-linked-list/main.py
@@ -0,0 +1,19 @@
+# coding:utf8
+# https://leetcode.com/problems/reverse-linked-list/
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """TODO 迭代 iteratively"""
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """TODO 递归 recursively"""
