13f71ad1e5493a4b1287b91923c763a3433c4a0d
pyq <pyq225@gmail.com>
2020-03-17 16:42:21 +0800

    add linux programming ch12

---
+++ learn-cpp/linux程序设计/ch11/alarm.c
@@ -0,0 +1,48 @@
+// 模拟一个闹钟
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static int alarm_fired = 0;
+
+void ding(int sig)
+{
+    alarm_fired = 1;
+}
+
+// main 函数中，告诉子进程 5 秒后发送一个 SIGALRM 信号给它的父进程
+int main()
+{
+    pid_t pid;
+
+    printf("alarm application starting\n");
+
+    pid = fork();
+
+    switch (pid)
+    {
+    case -1:
+        // failure
+        perror("fork failed");
+        exit(1);
+    case 0:
+        // child
+        sleep(5);
+        kill(getppid(), SIGALRM);
+        exit(0);
+    }
+
+    // if we get here we are the parent process
+    printf("waiting for alarm to go off\n");
+    (void)signal(SIGALRM, ding);
+
+    pause();
+    if (alarm_fired)
+        printf("Ding!\n");
+
+    printf("done\n");
+    exit(0);
+}
